447a138cb74c7d5dede119f0afacada2c6f366bb
Vineeth <vineethvs1927@gmail.com>
2025-03-19 22:10:49 +0530

    CI/CD rules updated

--- src/app/lib/mongodb.ts
+++ src/app/lib/mongodb.ts
@@ -3,6 +3,12 @@ import "dotenv/config";
 import { configDotenv } from "dotenv";
 configDotenv({ path: ".env.local" });
 
+const mongoUri = process.env.MONGO_URI_ATLAS || 'mongodb+srv://vineethvs1927:6TRgQ75lvH67BV32@cluster0.wtkmo.mongodb.net/closer?retryWrites=true&w=majority&appName=Cluster0';
+if (!mongoUri) {
+  throw new Error("MONGO_URI_ATLAS is not set. Check your environment variables.");
+}
+//const client = new MongoClient(mongoUri);
+
 const uri = process.env.MONGO_URI_ATLAS as string; // Ensure you have this in .env
 const options = {};
 
@@ -14,7 +20,7 @@ declare global {
 }
 
 if (!global._mongoClientPromise) {
-  client = new MongoClient(uri, options);
+  client = new MongoClient(mongoUri, options);
   global._mongoClientPromise = client.connect();
 }
 
